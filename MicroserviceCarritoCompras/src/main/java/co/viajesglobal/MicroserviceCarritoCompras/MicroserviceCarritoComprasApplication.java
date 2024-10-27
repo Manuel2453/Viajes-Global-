c8f5a9640e8b4563554c9a18bc0e85d55a302784
Valeb22 <105802946+Valeb22@users.noreply.github.com>
2024-10-27 18:57:11 -0500

    Microservicio Carrito de compras

---
+++ MicroserviceCarritoCompras/src/main/java/co/viajesglobal/MicroserviceCarritoCompras/MicroserviceCarritoComprasApplication.java
@@ -0,0 +1,13 @@
+package co.viajesglobal.MicroserviceCarritoCompras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceCarritoComprasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceCarritoComprasApplication.class, args);
+	}
+
+}
