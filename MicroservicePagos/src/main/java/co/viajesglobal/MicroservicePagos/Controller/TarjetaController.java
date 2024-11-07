4eda63625808e7c67269425e8a48919d9fc02d8a
ManuelTerreros <114838362+ManuelTerreros@users.noreply.github.com>
2024-11-06 19:02:57 -0500

    Cambios Tarjeta
    Se realizó la lógica pa las tarjetas

--- MicroservicePagos/src/main/java/co/viajesglobal/MicroservicePagos/Controller/TarjetaController.java
+++ MicroservicePagos/src/main/java/co/viajesglobal/MicroservicePagos/Controller/TarjetaController.java
@@ -1,9 +1,62 @@
 package co.viajesglobal.MicroservicePagos.Controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import co.viajesglobal.MicroservicePagos.DTO.TarjetaDTO;
+import co.viajesglobal.MicroservicePagos.Service.TarjetaService;
+
+@RestController
+@RequestMapping("/tarjeta")
 public class TarjetaController {
-
-	public TarjetaController() {
-		// TODO Auto-generated constructor stub
+	
+	/*
+	 Valentina, necesito que en el frontend, porfa, en el formulario hagas que la fecha se pida Mes/Año, en tipo String
+	 para que pruebes es con esto en postman:
+	 {
+    "numero_Tarjeta":"11111111111111111",
+    "nomtitular":"Pepito",
+    "Id_Cliente": 1,
+    "cvv":123,
+    "fechaVencimiento":"12/24"
+    }
+    la verdad, no sé pq no reconoce el pinche cliente, llevo rato tratando y na y me mamé
+	*/
+	
+	@Autowired
+	private TarjetaService tarjetaServicio;
+	
+	// Lo que quiero hacer con esto es que aparezca una lista desplegable para poder mostrar las tarjetas guardadas por un cliente
+	@GetMapping("/pruebalista")
+	public List<String> listarTarjetas(@RequestParam int idCliente){
+		List<String> resultado = tarjetaServicio.listarporCliente(idCliente);
+		return resultado;
+		
 	}
-
+	
+	// Imagina que desde el carrito le dan a pagar, entonces aparece un resumen y un espacio para ingresar los datos de la tarjeta, 
+	// entonces sería chevere que deje guardar la tarjeta con un checkbox o que con la lista de arriba, se llene los campos automáticamente
+	// luego que ahí si pase a generar el pago(eso casi que se hace solo)
+	@PostMapping("/insertarTarjeta")
+	public ResponseEntity<?> crearTarjeta(@RequestBody TarjetaDTO datos, @RequestParam boolean confirmacion ) {// lo que quiero hacer con confirmación es si a partir de un boton 
+																					// o un checkbox, se guarde la tarjeta
+	try {
+		TarjetaDTO resultado = tarjetaServicio.crearRegistroTarjeta(datos, confirmacion);
+		return new ResponseEntity<>(resultado, HttpStatus.CREATED);
+		
+	}catch(Exception e){
+		return new ResponseEntity<>("Ha ocurrido un error", HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+	
+	
 }
--- MicroservicePagos/src/main/java/co/viajesglobal/MicroservicePagos/Respository/TarjetaRepository.java
+++ MicroservicePagos/src/main/java/co/viajesglobal/MicroservicePagos/Respository/TarjetaRepository.java
@@ -1,9 +1,15 @@
 package co.viajesglobal.MicroservicePagos.Respository;
 
-public class TarjetaRepository {
+import java.util.List;
 
-	public TarjetaRepository() {
-		// TODO Auto-generated constructor stub
-	}
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import co.viajesglobal.MicroservicePagos.Entitys.PagoEntity;
+import co.viajesglobal.MicroservicePagos.Entitys.TarjetaEntity;
+
+public interface TarjetaRepository extends JpaRepository<TarjetaEntity, String>{
+	@Query(value = "SELECT Numero_Tarjeta FROM datos_tarjeta WHERE Id_Cliente = :idCliente", nativeQuery = true)
+	List<String> listarporCliente(@Param("idCliente")int idCliente);
 }
