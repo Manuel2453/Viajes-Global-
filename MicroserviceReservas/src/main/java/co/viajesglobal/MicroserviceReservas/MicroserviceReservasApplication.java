9914f411c45a6060cd8992aea74563bb2985d5e5
Valeb22 <105802946+Valeb22@users.noreply.github.com>
2024-11-11 20:38:17 -0500

    Microservicio de reservas

---
+++ MicroserviceReservas/src/main/java/co/viajesglobal/MicroserviceReservas/MicroserviceReservasApplication.java
@@ -0,0 +1,13 @@
+package co.viajesglobal.MicroserviceReservas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceReservasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceReservasApplication.class, args);
+	}
+
+}
