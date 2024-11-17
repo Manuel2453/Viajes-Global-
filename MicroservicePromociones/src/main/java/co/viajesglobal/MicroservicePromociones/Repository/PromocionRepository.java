7be646b450d8a0716f9b3bd674756a83c815729a
Manuel2453 <140219825+Manuel2453@users.noreply.github.com>
2024-11-17 14:42:14 -0500

    Delete MicroservicePromociones directory

--- MicroservicePromociones/src/main/java/co/viajesglobal/MicroservicePromociones/Repository/PromocionRepository.java
+++
@@ -1,8 +0,0 @@
-package co.viajesglobal.MicroservicePromociones.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import co.viajesglobal.MicroservicePromociones.Entity.Promocion;
-
-public interface PromocionRepository extends JpaRepository<Promocion, Integer> {
-}
